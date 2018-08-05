3b2ee6b53d15e70e9f19120c3741951bef64a979
Emily Welsch <emwelsch@gmail.com>
2018-08-05 00:22:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get 'name' do
+    "My name is __"
+  end
 
-name
-hometown
-favorite-song
-The name route should display "My name is __" in the browser, 
-the hometown route should display "My hometown is __", 
-and the favorite-song route should display "My favorite song is __".
+  get 'hometown' do
+    "My hometown is __"
+  end
+
+  get 'favorite-song' do
+    "My favorite song is __"
+  end
+
 end
